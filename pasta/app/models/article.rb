cd6e04a6376630dbbc02872010328c5ac829fb85
Riccardo Carlesso <palladiusbonton+derek.zrh.corp.google.com@gmail.com>
2018-02-09 16:24:48 +0100

    108

--- pasta/app/models/article.rb
+++ pasta/app/models/article.rb
@@ -3,4 +3,8 @@ class Article < ApplicationRecord
   has_many :comments, dependent: :destroy
   validates :title, presence: true, length: { minimum: 5 }
 
+  def number_of_comments
+  	self.comments.count
+  end
+
 end
